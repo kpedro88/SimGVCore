dbff30f520d40f60d293e9c167b6240d4304cfba
Kevin Pedro <kpedro88@gmail.com>
2019-09-04 13:52:58 -0500

    add log parsing for mem etc.

--- Application/test/get_contributions.py
+++ Application/test/get_contributions.py
@@ -2,7 +2,16 @@ from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 from collections import OrderedDict
 import json
 
-def get_contributions(report, geant, ptype):
+def parseLogVal(line,item=""):
+    if len(item)==0: return float(line.split(' ')[-1])
+    else:
+        linesplit = line.split(' ')
+        if item in linesplit:
+            return float(linesplit[linesplit.index(item)+1])
+        else:
+            return 0.
+
+def get_contributions(report, log, geant, ptype):
     geants = {
         "G4": "G4EventManager::DoProcessing",
         "GV": "geant::cxx::WorkloadManager::TransportTracksTask",
@@ -13,6 +22,12 @@ def get_contributions(report, geant, ptype):
         ("scoring","update(sim::StepWrapper"),
         ("output","edm::PoolOutputModule::write"),
     ])
+    stategories = [
+        "time",
+        "cpu",
+        "vsize",
+        "rss",
+    ]
 
     contributions = OrderedDict([(key,0.0) for key in categories])
     contributions["other"] = 0.0
@@ -29,21 +44,47 @@ def get_contributions(report, geant, ptype):
     skipped = ["total","scoring"]
     contributions["other"] = contributions["total"] - sum([cont for key,cont in contributions.iteritems() if key not in skipped])
 
-    # print results
-    if ptype=="text":
-        for key,cont in contributions.iteritems():
-            print "{0}: {1:.2f}".format(key,cont)
-    elif ptype=="json":
-        print json.dumps(contributions)
+    # parse log for mem info and overall time
+    stats = OrderedDict([(key,0.0) for key in stategories])
+    if len(log)>0:
+        with open(log,'r') as infile:
+            for line in infile:
+                # cleanup
+                line = line.rstrip()
+                if len(line)>0 and line[-1]==' ': line = line[:-1]
+                if len(line)==0: continue
 
-    return contributions
+                elif "Total loop" in line:
+                    if stats["time"]==0.0:
+                        stats["time"] = parseLogVal(line)
+                    else:
+                        stats["cpu"] = parseLogVal(line)
+                elif "VSIZE" in line or "RSS" in line:
+                    if "VSIZE" in line:
+                        stats["vsize"] = parseLogVal(line,"VSIZE")
+                    if "RSS" in line:
+                        stats["rss"] = parseLogVal(line,"RSS")
+
+    # print results
+    results = OrderedDict([("contributions", contributions)])
+    if len(log)>0: results["stats"] = stats
+    if ptype=="text":
+        for res,vals in results.iteritems():
+            print res
+            for key,cont in vals.iteritems():
+                print "{0}: {1:.2f}".format(key,cont)
+    elif ptype=="json":
+        print json.dumps(results)
+
+    return results
 
 if __name__=="__main__":
     # check arguments
     parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
     parser.add_argument("-g","--geant", dest="geant", type=str, default="", choices=["G4","GV"], help="geant version", required=True)
     parser.add_argument("-r", "--report", dest="report", type=str, default="", help="igprof report", required=True)
-    parser.add_argument("-p","--print", dest="ptype", type=str, default="text", choices=["","text","json"], help="how to print output", required=True)
+    parser.add_argument("-l", "--log", dest="log", type=str, default="", help="CMSSW output log")
+    parser.add_argument("-p","--print", dest="ptype", type=str, default="text", choices=["","text","json"], help="how to print output")
     args = parser.parse_args()
 
-    get_contributions(args.report, args.geant, args.ptype)
+    get_contributions(args.report, args.log, args.geant, args.ptype)
