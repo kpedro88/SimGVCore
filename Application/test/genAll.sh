6bfe84ec33374538d7abebc3d032ef308f78cc67
Kevin Pedro <kpedro88@gmail.com>
2019-09-04 17:12:43 -0500

    remove more old scripts

--- Application/test/genAll.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for particle in electron photon; do
-	for mult in 1 2 5 10 25 50 100; do
-		for pt in 10 25 50 100; do
-			cmsRun runGen.py particle=${particle} mult=${mult} pt=${pt} maxEvents=1000 >& log_${particle}_mult${mult}_pt${pt}_n1000.log
-		done
-	done
-done
