e08424120a0465925adc5405f26ac758ebc0a97b
Kevin Pedro <kpedro88@gmail.com>
2019-09-25 15:23:01 -0500

    single track mode off by default

--- Application/test/runSim.py
+++ Application/test/runSim.py
@@ -178,7 +178,7 @@ elif options.sim=="GV":
         HepMCProductLabel = cms.InputTag("generatorSmeared"),
         geometry = cms.string(""),
         ZFieldInTesla = cms.double(options.bfield),
-        singleTrackMode = cms.bool(True),
+        singleTrackMode = cms.bool(False),
         Scoring = scoring_,
     )
 
